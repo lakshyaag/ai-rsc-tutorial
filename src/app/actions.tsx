df1a746fcf19ccc8c09eca44465f7789172927ff
Lakshya Agarwal <lakshyajannu@gmail.com>
2024-08-28 00:19:04 -0400

    add ai sdk and chat input

---
+++ src/app/actions.tsx
@@ -0,0 +1,42 @@
+"use server";
+
+import { createAI } from "ai/rsc";
+import type { ReactNode } from "react";
+import type { ToolInvocation } from "ai";
+
+// system-message
+const content = `\
+You are a crypto bot and you can help users get the prices of cryptocurrencies.
+
+Messages inside [] means that it's a UI element or a user event. For example:
+- "[Price of BTC = 69000]" means that the interface of the cryptocurrency price of BTC is shown to the user.
+
+If the user wants the price, call \`get_crypto_price\` to show the price.
+If the user wants the market cap or other stats of a given cryptocurrency, call \`get_crypto_stats\` to show the stats.
+If the user wants a stock price, it is an impossible task, so you should respond that you are a demo and cannot do that.
+If the user wants to do anything else, it is an impossible task, so you should respond that you are a demo and cannot do that.
+
+Besides getting prices of cryptocurrencies, you can also chat with users.
+`;
+
+export const sendMessages = async () => {};
+
+export type AIState = Array<{
+	id?: "number";
+	name?: "get_crypto_price" | "get_crypto_stats";
+	role: "human" | "assistant" | "system";
+	content: string;
+}>;
+
+export type UIState = Array<{
+	id?: "number";
+	role: "human" | "assistant";
+	display: ReactNode;
+	toolInvocations?: ToolInvocation[];
+}>;
+
+export const AI = createAI({
+	initialAIState: [] as AIState,
+	initialUIState: [] as UIState,
+	actions: { sendMessages },
+});
---
+++ src/components/chat-list.tsx
@@ -0,0 +1,21 @@
+import type { UIState } from "@/app/actions";
+
+interface MessagesProps {
+	messages: UIState;
+}
+
+export function ChatList({ messages }: MessagesProps) {
+	if (!messages.length) {
+		return null;
+	}
+
+	return (
+		<div className="relative mx-auto max-w-2xl px-4">
+			{messages.map((message) => (
+				<div key={message.id} className="pb-4">
+					{message.display}
+				</div>
+			))}
+		</div>
+	);
+}
